6f04e6840a9474f038ee28865e49a1cf03988be9
alu0101329161 <alu0101329161@ull.edu.es>
2021-11-12 14:04:14 +0000

    Todo correcto

--- Rakefile
+++ Rakefile
@@ -6,3 +6,8 @@ task :re do
   sh "ruby -I. test/tc_regular_expression.rb"
 end
 
+desc "Pruebas simple"
+task :simple do
+   sh "ruby -I. test/tc_regular_expression.rb --name /simple/"
+end
+
--- test/tc_regular_expression.rb
+++ test/tc_regular_expression.rb
@@ -5,6 +5,9 @@ class TestRegularExpression < Test::Unit::TestCase
   def setup
     @origen = RegularExpression.new("a")
     @origen1 = RegularExpression.new("b")
+    @origen2 = RegularExpression.new("c")
+    @origen3 = RegularExpression.new("d")
+
   end
 
   def test_simple
@@ -17,16 +20,22 @@ class TestRegularExpression < Test::Unit::TestCase
   def test_simple_concatenacion
 	#Comprobar concatenacion
 	assert_equal("a.b", @origen.concat(@origen1).to_s)
+	assert_equal("b.c", @origen1.concat(@origen2).to_s)
+	assert_equal("c.d", @origen2.concat(@origen3).to_s)
   end
 
    def test_simple_altenativa
         #Comprobar alternativa
         assert_equal("a|b", @origen.alt(@origen1).to_s)
+	assert_equal("b|c", @origen1.alt(@origen2).to_s)
+	assert_equal("c|d", @origen2.alt(@origen3).to_s)
   end
 
   def test_simple_kleen
         #Comprobar cierre de Kleen
         assert_equal("a*", (@origen.kleen).to_s)
+	assert_equal("b*", (@origen1.kleen).to_s)
+	assert_equal("c*", (@origen2.kleen).to_s)
   end
 
 
